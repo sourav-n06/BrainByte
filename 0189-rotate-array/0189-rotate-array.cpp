600545b1af0e5fc7045733c8323aca404403f639
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-03-13 13:56:14 +0530

    Time: 0 ms (100%), Space: 29.3 MB (97.3%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,58 @@
+
+class Solution {
+    void Reverse(vector<int>& nums, int left, int right) {
+        int siz = right - left + 1;
+        for(int i = 0; i < siz/2; i++){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int rem = k % n;
+        
+
+        Reverse(nums, 0, n-1);
+        Reverse(nums, 0, rem - 1);
+        Reverse(nums, rem , n-1);
+
+    }
+};
+
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int N = nums.size();
+//         int rem = k % N;
+//         if (rem == 0) return;
+
+//         vector<int> vec(nums.end() - rem, nums.end());
+//         nums.erase(nums.end() - rem, nums.end());
+//         nums.insert(nums.begin(), vec.begin(), vec.end());
+//         return;
+//     }
+// };
+
+
+    // void rotate(vector<int>& nums, int k) {
+    //     int N = nums.size();
+    //     int rem = k % N;
+    //     if(rem == 0) return;
+
+    //     vector<int> vec;
+    //     int slow = N - rem;
+    //     while(slow != nums.size()) {
+    //         vec.push_back(nums[slow]);
+    //         cout<<slow;
+    //         nums.erase(nums.begin() + slow);
+    //     }
+
+    //     nums.insert(nums.begin(), vec.begin(), vec.end());
+    //     return;
+    // }

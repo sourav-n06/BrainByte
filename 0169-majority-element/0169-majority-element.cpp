c929332300e65553430e9edbbc7efef7491346f4
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-03-13 12:08:14 +0530

    Time: 0 ms (100%), Space: 28.2 MB (58.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,13 +4,17 @@ public:
         int ele = nums[0];
         int cnt = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(ele == nums[i]) cnt++;
-            else cnt--;
-
             if(cnt == 0) {
-                ele = nums[i + 1];
-            }
+                ele = nums[i];
+                cnt = 1;
+            } else if(ele == nums[i]) cnt++;
+            else cnt--;
         }
-        return ele;
+        cnt = 0;
+        for(auto x : nums) {
+            if(x == ele) cnt++;
+        }
+        if(cnt > nums.size() / 2) return ele;
+        else return -1;
     }
 };

77c372b48a99b19ea02ab2e8d8c11759c4416591
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-03-13 12:19:23 +0530

    Time: 0 ms (100%), Space: 28 MB (97.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //Moor's voting algo
         int ele = nums[0];
         int cnt = 0;
         for(int i = 0; i < nums.size(); i++) {

fe07fc13369d30a5e2a3c18301af63fe97b1976f
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-06-08 23:45:17 +0530

    Time: 3 ms (45.99%), Space: 14.3 MB (44.11%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    void dfs(int m, int n, int ele, vector<int>& res) {
+    void dfs(int n, int ele, vector<int>& res) {
         res.push_back(ele);
 
         if (n >= 9) {
-            for (int i = 0; i <= m; i++) {
+            for (int i = 0; i <= 9; i++) {
                 int new_ele = ele * 10 + i;
                 if (new_ele <= n)
-                    dfs(m, n, new_ele, res);
+                    dfs(n, new_ele, res);
             }
 
         } else {
             for (int i = 0; i <= n; i++) {
                 int new_ele = ele * 10 + i;
                 if (new_ele <= n)
-                    dfs(m, n, new_ele, res);
+                    dfs(n, new_ele, res);
             }
         }
     }
@@ -23,10 +23,10 @@ public:
         vector<int> res;
         if (n >= 9) {
             for (int i = 1; i <= 9; i++)
-                dfs(9, n, i, res);
+                dfs(n, i, res);
         } else {
             for (int i = 1; i <= n; i++)
-                dfs(9, n, i, res);
+                dfs(n, i, res);
         }
 
         return res;

95848205f138b6f5fffc7caf5e6a25f44cdf9705
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-10-19 22:44:59 +0530

    Time: 2 ms (76.26%), Space: 22.2 MB (63.49%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    bool checkByPart(string& s, int left, int right) {
+        while(left < right) {
+            if(s[left] != s[right]) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while(left <= right) {
+            if (s[left] == s[right]) {
+                left++; right--;
+            } else {
+                if(checkByPart(s, left+1, right) || checkByPart(s, left, right-1))
+                    return true;
+                else return false;
+            }
+        }
+        return true;
+    }
+};

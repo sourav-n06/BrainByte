09e12c4bf22201b2240974d821f69fc651052540
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-10-20 02:47:08 +0530

    Time: 0 ms (100%), Space: 21.9 MB (37.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+
+        int xorr = 0;
+
+        for(int i = 0; i <= nums.size(); i++) {
+            xorr ^= i;
+        }
+
+        for(int n : nums) {
+            xorr ^= n;
+        }
+
+        return xorr;
+    }
+};
+
+
+
+/*
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int total = n * (n + 1) / 2;
         int sum = 0;
@@ -9,3 +31,5 @@ public:
         return total - sum;
     }
 };
+
+*/

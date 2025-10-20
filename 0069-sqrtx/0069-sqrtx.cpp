d74af679d7ad0ad1c3a485aaf5cc8ab53aa33df8
Sourav Hazra <159522158+sourav-n06@users.noreply.github.com>
2025-10-20 21:31:04 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0 || x == 1) return x; // Base cases
+        int left = 0, right = x;
 
-        int low = 1, high = x, ans = 0;
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            long long square = mid * mid;
-            
-            if (square == x) return mid;   // Found exact square root
-            else if (square < x) {         // Move right, store possible answer
-                ans = mid;
-                low = mid + 1;
-            } else {                        // Move left
-                high = mid - 1;
-            }
+        while(left <= right) {
+            long long mid = (left + right) / 2;
+            if(mid * mid == x) return mid;
+            else if(mid * mid > x) right = mid - 1;
+            else left = mid + 1;
         }
-        return ans; // Integer part of sqrt(x)
+
+        return left-1;
     }
 };
